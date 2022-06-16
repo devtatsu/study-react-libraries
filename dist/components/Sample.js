a36ce176dddf82f3c0a3d5472781991c8501a659
OshitaTatsuya <oshita.tatsuya@tis.co.jp>
2022-06-17 06:32:05 +0900

    fix config setting

---
+++ dist/components/Sample.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Sample = void 0;
+const react_1 = require("react");
+const Sample = () => {
+    return (react_1.default.createElement("div", null, "sample test comp"));
+};
+exports.Sample = Sample;
